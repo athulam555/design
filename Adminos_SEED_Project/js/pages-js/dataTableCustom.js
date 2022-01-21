d92925f58f76fdd762b6ca83f04bd2e9da24131e
Athul AM <athulam555@gmail.com>
2022-01-21 16:54:38 +0530

    fixxxx

--- Adminos_SEED_Project/js/pages-js/dataTableCustom.js
+++ Adminos_SEED_Project/js/pages-js/dataTableCustom.js
@@ -499,8 +499,65 @@ $(document).ready(function() {
     $('#base-style').DataTable();
     $('#no-style').DataTable();
     $('#compact').DataTable();
-    $('#table-style-hover').DataTable();
+    // $('#table-style-hover').DataTable();
     // Styling js end
+
+    $('#table-style-hover thead tr')
+        .clone(true)
+        .addClass('filters')
+        .appendTo('#table-style-hover thead');
+        
+    var table = $('#table-style-hover').DataTable({
+        orderCellsTop: true,
+        fixedHeader: true,
+        initComplete: function () {
+            var api = this.api();
+ 
+            // For each column
+            api
+                .columns([0,1,2])
+                .eq(0)
+                .each(function (colIdx) {
+                    // Set the header cell to contain the input element
+                    var cell = $('.filters th').eq(
+                        $(api.column(colIdx).header()).index()
+                    );
+                    var title = $(cell).text();
+                    $(cell).html('<input type="text" class="form-control"   placeholder="' + title + '" />');
+ 
+                    // On every keypress in this input
+                    $(
+                        'input',
+                        $('.filters th').eq($(api.column(colIdx).header()).index())
+                    )
+                        .off('keyup change')
+                        .on('keyup change', function (e) {
+                            e.stopPropagation();
+ 
+                            // Get the search value
+                            $(this).attr('title', $(this).val());
+                            var regexr = '({search})'; //$(this).parents('th').find('select').val();
+ 
+                            var cursorPosition = this.selectionStart;
+                            // Search the column for that value
+                            api
+                                .column(colIdx)
+                                .search(
+                                    this.value != ''
+                                        ? regexr.replace('{search}', '(((' + this.value + ')))')
+                                        : '',
+                                    this.value != '',
+                                    this.value == ''
+                                )
+                                .draw();
+ 
+                            $(this)
+                                .focus()[0]
+                                .setSelectionRange(cursorPosition, cursorPosition);
+                        });
+                });
+        },
+    });
 
 
     $('#simple-table').DataTable();
